ae171ff3b748f9ae52070f4634da1b6e0996bcb0
Nileshwar-Paul-au26 <776paul@gmail.com>
2022-05-21 22:50:21 +0530

    cloudinary config

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ const app = express();
 app.set("view engine", "ejs");
 let imgurl;
 let PORT = process.env.PORT || 8000
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret,
+    secure: true,
+});
+//Storing Files uploaded by User Inmemory(not in disk because we want the buffer format data of the file) through multer
+const uploader = multer({ storage: multer.memoryStorage() });
+app.listen(PORT, console.log(`Server listening on Port ${process.env.PORT}`));
